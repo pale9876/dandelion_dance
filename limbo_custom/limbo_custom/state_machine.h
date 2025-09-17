443733c13e4baf2367f6b8d165ffd9f2de306c6d
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2025-09-17 09:50:34 +0900

    1
    1

--- limbo_custom/limbo_custom/state_machine.h
+++ limbo_custom/limbo_custom/state_machine.h
@@ -4,12 +4,32 @@
 #define STATE_MACHINE_H
 
 #include "hsm/limbo_hsm.h"
+#include "hsm/limbo_state.h"
+
+#include <godot_cpp/variant/typed_dictionary.hpp>
+
 
 class StateMachine : public LimboHSM
 {
-
+    
     protected:
     static void _bind_methods();
+
+    public:
+    const StringName IDLE_STATE = "Idle";
+    const StringName MOVE_STATE = "Move";
+    const StringName JUMP_STATE = "Jump";
+    const StringName DRAG_STATE = "Drag";
+    const StringName DASH_STATE = "Dash";
+    const StringName FALL_STATE = "Fall";
+    
+    TypedDictionary<StringName, Node> states = {};
+    TypedDictionary<StringName, Node> individual_acts = {};
+
+    void _ready() override;
+
+    bool add_state(Node* state);
+    Node* get_state(StringName state_name);
 
 };
 
