3ea2d5d87dec0dc707fe1608b36b485daa2fb583
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2025-09-17 09:56:25 +0900

    1
    1

--- limbo_custom/limbo_custom/state_machine.cpp
+++ limbo_custom/limbo_custom/state_machine.cpp
@@ -4,29 +4,3 @@ void StateMachine::_bind_methods()
 {
 
 }
-
-void StateMachine::_ready()
-{
-
-	TypedArray<Node> childs = this -> get_children();
-
-	for (Variant v : childs)
-	{
-		LimboState* state= Object::cast_to<LimboState>(v);
-		if (state)
-		{
-				
-		}
-	}
-
-}
-
-bool StateMachine::add_state(Node* state)
-{
-	return false;
-}
-
-Node* StateMachine::get_state(StringName state_name)
-{
-	return nullptr;
-}
