c12e68a69b3a9a0bc5ff144561d3e89bc42ff368
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2025-10-13 02:14:07 +0900

    1
    1

--- detonator_2/cs_classes/refs/Trigger.cs
+++ detonator_2/cs_classes/refs/Trigger.cs
@@ -9,13 +9,13 @@ public partial class Trigger : Resource
     {
         WORK = 0,
         TARGET_NULL = 1,
-        OUT_OF_RANGE = 2,
-        NOT_CONTAINS = 3,
+        INVAILD_PROPERTY = 2,
     }
 
     [Export] public String trigger_name = "";
     [Export] public bool keep_activate = false;
     [Export] public bool keep_target = false;
+
     public ErrReason err_reason = ErrReason.WORK;
 
     public virtual bool activate(Node2D target)
